8c9df5d322391f6f71fe9c4b184230a68053d484
Raiet Intraub <raietsaeed@gmail.com>
2018-06-02 19:53:41 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,8 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
 
-    # NOTE : No longer needed 
-    # author = @post.author
+    # NOTE : No longer needed since using locals in view
+    # @ author = @post.author
   end
 
   def index
